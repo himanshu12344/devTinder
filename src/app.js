ed0083ba7004ece7c4d9013ea74c0a976a8eb83c
himanshu12344 <himanshuadhana007@gmail.com>
2025-06-17 09:25:25 +0530

    exploring get, post, delete api's on postman

--- src/app.js
+++ src/app.js
@@ -2,12 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello world");
+// app.use("/test", (req, res) => {
+//   res.send("Namaste");
+// });
+
+// This will only handle GET request for /user
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Himanshu", lastname: "Adhana" });
 });
 
-app.use("/test", (req, res) => {
-  res.send("Namaste");
+//This will only handle POST request for / user
+app.post("/user", (req, res) => {
+  res.send("Sending user data to DB");
+});
+
+// This will handle DELETE request
+app.delete("/user", (req, res) => {
+  res.send("Delete user Data");
 });
 
 app.listen(7777, () => {
